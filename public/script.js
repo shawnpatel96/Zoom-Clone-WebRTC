f61ebd2821346cb634bfab16350f6b0d1e1478c1
shawn <shawnpatel196@gmail.com>
2020-08-20 16:29:06 -0500

    added video funcitonalty

--- public/script.js
+++ public/script.js
@@ -1,10 +1,33 @@
 const socket = io('/')
-const myPeer = new myPeer(undefined, {
+const videoGrid = document.getElementById('video-grid')
+const myPeer = new Peer(undefined, {
     host:'/',
     port: '3001'
 })
-socket.emit('join-room', ROOM_ID, 10)
+const myVideo = document.createElement('video')
+myVideo.muted=true
+
+navigator.mediaDevices.getUserMedia({
+    video:true,
+    audio:true
+
+}).then(stream=>{
+    addVideoStream(myVideo,stream)
+})
+myPeer.on('open',id=>{
+    socket.emit('join-room', ROOM_ID, id)
+})
+
 
 socket.on('user-connected', userId =>{
     console.log('User has connected:' + userId)
 })
+
+function addVideoStream(video, stream){
+    video.srcObject = stream
+    video.addEventListener('loadedmetadata', ()=>{
+        video.play()
+
+    })
+    videoGrid.append(video)
+}
